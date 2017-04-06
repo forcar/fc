e88c517cebb4ff6b6b23a3d55b98f2976171621c
lcsmith <lcsmith@clonsl2.jlab.org>
2017-04-06 10:33:25 -0400

    Convert OSTYPE to lower case

--- clas-fc/src/org/clas/fcmon/tools/MonitorApp.java
+++ clas-fc/src/org/clas/fcmon/tools/MonitorApp.java
@@ -145,8 +145,8 @@ public class MonitorApp extends JFrame implements ActionListener {
     } 
     
     public void getEnv() {        
-        String   ostype = System.getenv("OSTYPE");    
-        if (ostype!=null&&ostype.equals("Linux")) {
+        String   ostype = System.getenv("OSTYPE").toLowerCase();    
+        if (ostype!=null&&ostype.equals("linux")) {
             String hostname = System.getenv("HOST");
             if(hostname.substring(0,4).equals("clon")) {
               System.out.println("Running on "+hostname);
