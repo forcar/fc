27100e78ba0f899a6b5104956fe78b4a0b812de4
lcsmith <lcsmith@clonsl2.jlab.org>
2017-09-04 17:45:27 -0400

    Minor edit

--- clas-fc/src/org/clas/fcmon/ec/ECHvApp.java
+++ clas-fc/src/org/clas/fcmon/ec/ECHvApp.java
@@ -210,7 +210,7 @@ public class ECHvApp extends FCEpics implements ActionListener {
         for (int is=is1; is<is2 ; is++) {
             for (int il=il1; il<il2 ; il++) {
                 for (int ic=1; ic<nlayMap.get(detName)[il-1]+1; ic++) {
-                    System.out.println("is="+is+" il="+il+" ic="+ic+" HVnew="+app.fifo6.get(is, il, ic).getLast());
+                    System.out.println("Sec="+is+" Lay="+il+" PMT="+ic+" HVnew="+app.fifo6.get(is, il, ic).getLast());
                     putCaValue(0,"vset",is,il,ic,app.fifo6.get(is, il, ic).getLast());  
                 }
             }
