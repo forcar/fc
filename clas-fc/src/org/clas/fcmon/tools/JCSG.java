1fd237eaefa5d0a2c3477b9ea14d880dd9f62ea3
Cole Smith <lcs1956@me.com>
2019-05-05 15:40:43 -0400

    Added JCSG/CJ volume vertex mapping for U,V,W and PCAL,ECIN,ECOU

--- clas-fc/src/org/clas/fcmon/tools/JCSG.java
+++ clas-fc/src/org/clas/fcmon/tools/JCSG.java
@@ -24,21 +24,25 @@ public class JCSG {
     
     String          name[] = {"PCAL","ECin","ECout"};
     int             isec[] = {1,1,1};
-    int             ilay[] = {1,1,1};
+    int             ilay[] = {3,3,3};
     int             istr[] = {59,28,28}; //Normal radius from target at {59,28,28}
-    int[][]       vertices = {{4,0,5,1,7,3,6,2},{6,2,7,3,5,1,4,0},{6,2,7,3,5,1,4,0}};  //CJ vertices -> JCSG vertices
+
+    int[][]       vertices = {{4,0,5,1,7,3,6,2},{6,2,7,3,5,1,4,0},{4,0,5,1,7,3,6,2},  //JCSG vertices -> CJ vertices (PCAL)
+                              {4,0,5,1,7,3,6,2},{6,2,7,3,5,1,4,0},{1,5,0,4,2,6,3,7},  //JCSG vertices -> CJ vertices (ECIN)
+                              {4,0,5,1,7,3,6,2},{6,2,7,3,5,1,4,0},{1,5,0,4,2,6,3,7}}; //JCSG vertices -> CJ vertices (ECOU)
+                             //        U                V                 W
     int                cal = 0;
     
     public JCSG() {        
     	G4Trap vol = new G4Trap("vol", 1,0,0,1,1,1,0,1,1,1,0);
         for(int i=0;i<8;i++) System.out.println(getP3D(vol.getVertex(i)));
-        System.out.println("/geometry/ec/ec/dist2tgt = "+cp2.getDouble("/geometry/ec/ec/dist2tgt",0)+" mm");
-        System.out.println("SECTOR "+isec[0]+" LAYER "+ilay[0]+ " PCAL STRIP "+istr[0]+" EC STRIP "+istr[1]);
+        System.out.println("\n"+"/geometry/ec/ec/dist2tgt = "+cp2.getDouble("/geometry/ec/ec/dist2tgt",0)+" mm"+"\n");
+        System.out.println("SECTOR "+isec[0]+" LAYER "+ilay[0]+ " PCAL STRIP "+istr[0]+" EC STRIP "+istr[1]+"\n");
         processVert();
     }
         
     private void processVert() {
-        for (int cal=0; cal<3; cal++) {doJCSGVert();doCJVert(); }   	
+        for (cal=0; cal<3; cal++) {doJCSGVert();doCJVert(); System.out.println(" ");}   	
     }
     
     private void doCJVert() {
@@ -55,7 +59,7 @@ public class JCSG {
         switch (cal) {
         case 0: printJCSG(pcal.getPaddle(isec[0],ilay[0],istr[0])) ; break;
         case 1: printJCSG(ecal.getPaddle(isec[1],ilay[1],istr[1])) ; break;
-        case 2: printJCSG(ecal.getPaddle(isec[1],ilay[1]+15,istr[1])) ; 
+        case 2: printJCSG(ecal.getPaddle(isec[2],ilay[1]+15,istr[2])) ; 
         }
     }
     
@@ -64,17 +68,17 @@ public class JCSG {
             double x = vol.getVertex(i).x;
             double y = vol.getVertex(i).y;
             double z = vol.getVertex(i).z;
-            double r0 = Math.sqrt(x*x+z*z);
-            double r1 = Math.sqrt(x*x+y*y+z*z);
+            String r0 = String.format("%.4f",(float)Math.sqrt(x*x+z*z));
+            String r1 = String.format("%.4f",(float)Math.sqrt(x*x+y*y+z*z));
             System.out.println(getP3D(vol.getVertex(i))+"    Rxz = "+r0+"    Rxyz = "+r1); 
         }    	
     }
     
     private void printCJ(ScintillatorPaddle paddle) {
     	for(int j=0; j<8 ; j++) {
-    		point1.copy(paddle.getVolumePoint(vertices[ilay[cal]-1][j]));
-    		double r0 = Math.sqrt(point1.x()*point1.x()+point1.z()*point1.z());
-    		double r1 = Math.sqrt(point1.x()*point1.x()+point1.y()*point1.y()+point1.z()*point1.z());
+    		point1.copy(paddle.getVolumePoint(vertices[(ilay[cal]-1)+3*cal][j]));
+    		String r0 = String.format("%.4f",(float)Math.sqrt(point1.x()*point1.x()+point1.z()*point1.z()));
+    		String r1 = String.format("%.4f",(float)Math.sqrt(point1.x()*point1.x()+point1.y()*point1.y()+point1.z()*point1.z()));
     		System.out.println(point1+"    Rxz = "+r0+"    Rxyz = "+r1);	
     	}   	
     }
